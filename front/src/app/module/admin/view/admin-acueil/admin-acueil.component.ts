78cf56341b69bb2315e05f88ab2b1996fc8b9301
khalil1fs <aboulouanek@gmail.com>
2022-05-23 14:23:38 +0100

    admin acueil

---
+++ front/src/app/module/admin/view/admin-acueil/admin-acueil.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-acueil',
+  templateUrl: './admin-acueil.component.html',
+  styleUrls: ['./admin-acueil.component.css']
+})
+export class AdminAcueilComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
