96413c4410b1faeb4fbafa835bfec9a4e027b94c
khalil1fs <aboulouanek@gmail.com>
2022-05-25 13:24:49 +0100

    nav gearant

---
+++ front/src/app/module/agent/view/nav-gerant/nav-gerant.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-gerant',
+  templateUrl: './nav-gerant.component.html',
+  styleUrls: ['./nav-gerant.component.css']
+})
+export class NavGerantComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  sidebarToggle() {
+
+  }
+
+  logOut() {
+
+  }
+}
