cbda997a7b28b5cfa9bb88217316431a24c3dc92
khalil1fs <aboulouanek@gmail.com>
2022-05-27 11:54:44 +0100

    home client + redirect + Navbar client ( notification ..)

--- front/src/app/module/admin/view/user-admin/user-list/user-list.component.ts
+++ front/src/app/module/admin/view/user-admin/user-list/user-list.component.ts
@@ -2,6 +2,8 @@ import {Component, OnInit} from '@angular/core';
 import {User} from "../../../../../controller/model/User.model";
 import {UserService} from "../../../../../controller/service/User.service";
 import {Router} from "@angular/router";
+import {Customer, Representative} from "./custumer";
+import {CustomerService} from "./custumerservice";
 
 @Component({
     selector: 'app-user-list',
@@ -10,23 +12,81 @@ import {Router} from "@angular/router";
 })
 export class UserListComponent implements OnInit {
 
-    constructor(private router: Router, private userService: UserService) {
+    // cols: any[];
+    //
+    // constructor(private router: Router, private userService: UserService) {
+    // }
+    //
+    // ngOnInit(): void {
+    //     this.userService.findAll();
+    //     this.users;
+    //
+    //     this.cols = [
+    //         { field: 'vin', header: 'Vin' },
+    //         { field: 'year', header: 'Year' },
+    //         { field: 'brand', header: 'Brand' },
+    //         { field: 'color', header: 'Color' }
+    //     ];
+    //
+    // }
+    //
+    // delete(user: any) {
+    //
+    // }
+    //
+    // get users(): Array<User>{
+    //     return this.userService.users;
+    // }
+    //
+    // add() {
+    //     this.router.navigate(['/add-user'])
+    // }
+
+
+    customers: Customer[];
+
+    selectedCustomers: Customer[];
+
+    representatives: Representative[];
+
+    statuses: any[];
+
+    loading: boolean = true;
+
+    activityValues: number[] = [0, 100];
+
+    constructor(private customerService: CustomerService) { }
+
+    ngOnInit() {
+        this.customerService.getCustomersLarge().then(customers => {
+            this.customers = customers;
+            this.loading = false;
+
+            // @ts-ignore
+            this.customers.forEach(customer => customer.date = new Date(customer.date));
+        });
+
+        this.representatives = [
+            {name: "Amy Elsner", image: 'amyelsner.png'},
+            {name: "Anna Fali", image: 'annafali.png'},
+            {name: "Asiya Javayant", image: 'asiyajavayant.png'},
+            {name: "Bernardo Dominic", image: 'bernardodominic.png'},
+            {name: "Elwin Sharvill", image: 'elwinsharvill.png'},
+            {name: "Ioni Bowcher", image: 'ionibowcher.png'},
+            {name: "Ivan Magalhaes",image: 'ivanmagalhaes.png'},
+            {name: "Onyama Limba", image: 'onyamalimba.png'},
+            {name: "Stephen Shaw", image: 'stephenshaw.png'},
+            {name: "Xuxue Feng", image: 'xuxuefeng.png'}
+        ];
+
+        this.statuses = [
+            {label: 'Unqualified', value: 'unqualified'},
+            {label: 'Qualified', value: 'qualified'},
+            {label: 'New', value: 'new'},
+            {label: 'Negotiation', value: 'negotiation'},
+            {label: 'Renewal', value: 'renewal'},
+            {label: 'Proposal', value: 'proposal'}
+        ]
     }
 
-    ngOnInit(): void {
-        this.userService.findAll();
-        this.users;
-    }
-
-    delete(user: any) {
-
-    }
-
-    get users(): Array<User>{
-        return this.userService.users;
-    }
-
-    add() {
-        this.router.navigate(['/add-user'])
-    }
 }
---
+++ front/src/app/module/client/view/nav-client/nav-client.component.ts
@@ -0,0 +1,150 @@
+import {Component, ElementRef, OnInit} from '@angular/core';
+import {Location} from "@angular/common";
+
+@Component({
+  selector: 'app-nav-client',
+  templateUrl: './nav-client.component.html',
+  styleUrls: ['./nav-client.component.css']
+})
+export class NavClientComponent implements OnInit {
+  private toggleButton: any;
+  private sidebarVisible: boolean;
+
+  constructor(public location: Location, private element: ElementRef) {
+    this.sidebarVisible = false;
+  }
+
+  ngOnInit() {
+    const navbar: HTMLElement = this.element.nativeElement;
+    this.toggleButton = navbar.getElementsByClassName('navbar-toggler')[0];
+  }
+
+  sidebarOpen() {
+    const toggleButton = this.toggleButton;
+    const html = document.getElementsByTagName('html')[0];
+    // console.log(html);
+    // console.log(toggleButton, 'toggle');
+
+    setTimeout(function () {
+      toggleButton.classList.add('toggled');
+    }, 500);
+    html.classList.add('nav-open');
+
+    this.sidebarVisible = true;
+  };
+
+  sidebarClose() {
+    const html = document.getElementsByTagName('html')[0];
+    // console.log(html);
+    this.toggleButton.classList.remove('toggled');
+    this.sidebarVisible = false;
+    html.classList.remove('nav-open');
+  };
+
+  sidebarToggle() {
+    // const toggleButton = this.toggleButton;
+    // const body = document.getElementsByTagName('body')[0];
+    if (this.sidebarVisible === false) {
+      this.sidebarOpen();
+    } else {
+      this.sidebarClose();
+    }
+  };
+
+  isHome() {
+    var titlee = this.location.prepareExternalUrl(this.location.path());
+    // if(titlee.charAt(0) === '#'){
+    //     titlee = titlee.slice( 1 );
+    // }
+    if (titlee === '/home') {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  isDocumentation() {
+    var titlee = this.location.prepareExternalUrl(this.location.path());
+    if (titlee.charAt(0) === '#') {
+      titlee = titlee.slice(1);
+    }
+    if (titlee === '/documentation') {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  isindex() {
+    var titlee = this.location.prepareExternalUrl(this.location.path());
+    if (titlee.charAt(0) === '#') {
+      titlee = titlee.slice(1);
+    }
+    return titlee === '/home';
+  }
+
+  islogin() {
+    var titlee = this.location.prepareExternalUrl(this.location.path());
+    if (titlee.charAt(0) === '#') {
+      titlee = titlee.slice(1);
+    }
+    return titlee === '/login';
+  }
+
+  isMateriel() {
+    var titlee = this.location.prepareExternalUrl(this.location.path());
+    if (titlee.charAt(0) === '#') {
+      titlee = titlee.slice(1);
+    }
+    return titlee === '/produitbio';
+  }
+
+  isRegister() {
+    var titlee = this.location.prepareExternalUrl(this.location.path());
+    if (titlee.charAt(0) === '#') {
+      titlee = titlee.slice(1);
+    }
+    return titlee === '/register';
+  }
+
+  isCheckout() {
+    var titlee = this.location.prepareExternalUrl(this.location.path());
+    if (titlee.charAt(0) === '#') {
+      titlee = titlee.slice(1);
+    }
+    return titlee === '/checkout';
+  }
+
+  isPanier() {
+    var titlee = this.location.prepareExternalUrl(this.location.path());
+    if (titlee.charAt(0) === '#') {
+      titlee = titlee.slice(1);
+    }
+    return titlee === '/panier';
+  }
+
+  isFormation() {
+    var titlee = this.location.prepareExternalUrl(this.location.path());
+    if (titlee.charAt(0) === '#') {
+      titlee = titlee.slice(1);
+    }
+    return titlee === '/formation';
+  }
+
+  isCongres() {
+    var titlee = this.location.prepareExternalUrl(this.location.path());
+    if (titlee.charAt(0) === '#') {
+      titlee = titlee.slice(1);
+    }
+    return titlee === '/congres';
+  }
+
+  isOverseaeducation() {
+    var titlee = this.location.prepareExternalUrl(this.location.path());
+    if (titlee.charAt(0) === '#') {
+      titlee = titlee.slice(1);
+    }
+    return titlee === '/overseaseducation';
+  }
+
+}
