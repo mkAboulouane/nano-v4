834e8d17eb8596d9975d51836b2d1a64549b10e1
badr <badraithammou2000@gmail.com>
2022-05-31 12:08:24 +0100

    cursor and navbar collapse

--- front/src/app/module/agent/view/nav-gerant/nav-gerant.component.ts
+++ front/src/app/module/agent/view/nav-gerant/nav-gerant.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit} from '@angular/core';
 import {AuthService} from "../../../../controller/service/Auth.service";
+import {Location} from "@angular/common";
 
 @Component({
   selector: 'app-nav-gerant',
@@ -7,15 +8,49 @@ import {AuthService} from "../../../../controller/service/Auth.service";
   styleUrls: ['./nav-gerant.component.css']
 })
 export class NavGerantComponent implements OnInit {
+  private toggleButton: any;
+  private sidebarVisible: boolean;
+  constructor(private authService: AuthService, public location: Location, private element: ElementRef) { this.sidebarVisible = false;}
 
-  constructor(private authService: AuthService) { }
 
-  ngOnInit(): void {
+
+  ngOnInit() {
+    const navbar: HTMLElement = this.element.nativeElement;
+    this.toggleButton = navbar.getElementsByClassName('navbar-toggler')[0];
   }
+
+  sidebarOpen() {
+    const toggleButton = this.toggleButton;
+    const html = document.getElementsByTagName('html')[0];
+    // console.log(html);
+    // console.log(toggleButton, 'toggle');
+
+    setTimeout(function () {
+      toggleButton.classList.add('toggled');
+    }, 500);
+    html.classList.add('nav-open');
+
+    this.sidebarVisible = true;
+  };
+
+  sidebarClose() {
+    const html = document.getElementsByTagName('html')[0];
+    // console.log(html);
+    this.toggleButton.classList.remove('toggled');
+    this.sidebarVisible = false;
+    html.classList.remove('nav-open');
+  };
 
   sidebarToggle() {
+    // const toggleButton = this.toggleButton;
+    // const body = document.getElementsByTagName('body')[0];
+    if (this.sidebarVisible === false) {
+      this.sidebarOpen();
+    } else {
+      this.sidebarClose();
+    }
+  };
 
-  }
 
   logOut() {
     this.authService.logout();
