898d2bb32a67e306b243dac858691e33362da048
khalil1fs <aboulouanek@gmail.com>
2022-06-01 11:37:31 +0100

    add view Formation [id]

---
+++ front/src/app/module/client/view/view-formation/view-formation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-view-formation',
+  templateUrl: './view-formation.component.html',
+  styleUrls: ['./view-formation.component.css']
+})
+export class ViewFormationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
