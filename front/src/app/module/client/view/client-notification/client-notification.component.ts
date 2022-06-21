f8c67c8d5524f4bb10bc016472840c0930bfba4e
khalil fs1 <aboulouanek@gmail.com>
2022-06-21 10:38:11 +0100

    notification

--- front/src/app/module/client/view/client-notification/client-notification.component.ts
+++ front/src/app/module/client/view/client-notification/client-notification.component.ts
@@ -1,6 +1,8 @@
 import {Component, OnInit} from '@angular/core';
-import {UserService} from "../../../../controller/service/User.service";
-import {User} from "../../../../controller/model/User.model";
+import {UserService} from "app/controller/service/User.service";
+import {User} from "app/controller/model/User.model";
+import {NotificationService} from "app/controller/service/notification.service";
+import {Notification} from "app/controller/model/notification.model";
 
 @Component({
   selector: 'app-client-notification',
@@ -9,20 +11,67 @@ import {User} from "../../../../controller/model/User.model";
 })
 export class ClientNotificationComponent implements OnInit {
   user = new User();
-  constructor(private userService: UserService) { }
+  key = 0;
+  notifications: Notification[] = [];
+  constructor(private userService: UserService, private notificationService: NotificationService) { }
 
   ngOnInit(): void {
-    this.online();
+      this.online();
+      setInterval(e=>{
+            this.notification(this.user.id)
+      },5000)
   }
+
+  update(){
+      this.notificationService.update().subscribe(
+          data=> console.log(data)
+          , error => console.log(error)
+      );
+  }
+
+   notSeen() {
+      this.key = 0;
+      this.notifications.forEach(e=>{
+          if(!e.seen) this.key++;
+      }) 
+  }
+    
 
   online(){
     this.userService.currentUser().subscribe(
         data => {
           this.user = data;
-          console.log('current user'+data);
+            console.log('current user '+data);
+            this.notification(this.user.id);
         },error => console.log(error)
 
     )
   }
+
+    notification(id: number){
+        this.notificationService.findByUserId(id).subscribe(
+            data => {
+                this.notifications = data;
+                this.notifications = this.notifications.reverse();
+                this.notifications.forEach(e=>{
+                   this.selectedNotification = e;
+                   this.update();
+                });
+                this.notSeen();
+                console.log(data);
+            },error => console.log(error)
+        )
+    }
+
+    get selectedNotification(): Notification {
+      return this.notificationService.selectedNotification;
+    }
+
+    set selectedNotification(value: Notification){
+      this.notificationService.selectedNotification = value;
+    }
+
+
+
 
 }
